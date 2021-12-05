07236227d5231c3f5d58f8d5492ea8b90f7adb5c
Fahreza Ali Syahdani <thefahre@gmail.com>
2021-12-05 22:14:36 +0700

    Week 11 blank :'(

---
+++ Week11_33092/app/src/main/java/com/example/week11_33092/DataRepository.java
@@ -0,0 +1,15 @@
+package com.example.week11_33092;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class DataRepository {
+    static String placeholderUrl = "https://jsonplaceholder.typicode.com/";
+    static final PostServices create() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(placeholderUrl)
+                .build();
+        return retrofit.create(PostServices.class);
+    }
+}
---
+++ Week11_33092/app/src/main/java/com/example/week11_33092/PostServices.java
@@ -0,0 +1,11 @@
+package com.example.week11_33092;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface PostServices {
+    @GET("posts")
+    Call<List<PostModel>> getPosts();
+}
